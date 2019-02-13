d009d1e8f929f59769f3795bd23634868b301129
Manank Patni <manank321@gmail.com>
2019-02-14 03:58:22 +0530

    reun collectsttaic

--- renportal/settings.py
+++ renportal/settings.py
@@ -116,7 +116,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/assets')
 STATIC_URL = '/static/'
 LOGIN_URL = '/accounts/login/'
 LOGOUT_URL = '/accounts/login/'
