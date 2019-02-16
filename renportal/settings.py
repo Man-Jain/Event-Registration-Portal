09e79529db42efd1ee49b09a1f254e9af0092c81
Manank Patni <manank321@gmail.com>
2019-02-16 18:21:19 +0530

    Update settings.py

--- renportal/settings.py
+++ renportal/settings.py
@@ -75,10 +75,8 @@ WSGI_APPLICATION = 'renportal.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
-    #'default': dj_database_url.config(conn_max_age=500)
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),}
+    'default': dj_database_url.config(conn_max_age=500)
+
 }
 
 
